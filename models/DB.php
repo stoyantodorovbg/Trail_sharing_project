5593367bd3eeb5a842db539e135e1731aa7d7f4e
stoyantodorovbg <stoyan_11@abv.bg>
2017-10-22 11:06:31 +0300

    Create folder structure, add twig and autoload psr4, create connection with DB

---
+++ models/DB.php
@@ -0,0 +1,26 @@
+<?php
+
+
+class DB extends PDO
+{
+    private $db_host = 'localhost';
+    private $db_name = '';
+    private $db_user = 'root';
+    private $db_password = '';
+    private $db = false;
+
+
+
+    public function __construct() {
+        parent::__construct("mysql:dbname=".$this->db_name.";host=".$this->db_host, $this->db_user, $this->db_password);
+    }
+
+    public function setErrorException() {
+        $this->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    }
+
+    public function connect() {
+        $this->setErrorException();
+        return $this;
+    }
+}
