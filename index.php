03693a5561ae8a315b07ca9e3cf74ba76dfb841f
stoyantodorovbg <stoyan_11@abv.bg>
2017-10-22 14:26:50 +0300

    Initialize loader in index.php, add sample home page

--- index.php
+++ index.php
@@ -1,3 +1,5 @@
 <?php
+$loader = require __DIR__ . '/vendor/autoload.php';
+
 $app = new App();
 $app->main();
--- src/App.php
+++ src/App.php
@@ -5,6 +5,6 @@ class App
 {
     public function main()
     {
-
+        header('Location: src/templates/home/home.html');
     }
 }
